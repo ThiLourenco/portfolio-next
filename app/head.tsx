5b97cc8296aa0a53f375adfe2a41594557a89eae
Thigo <thi.it.solutions@gmail.com>
2023-08-05 23:12:50 -0300

    fix(): meta tag

--- app/head.tsx
+++ app/head.tsx
@@ -39,21 +39,21 @@ const Head = () => {
         content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
         name="robots"
       />
-      <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
-      <link href="/static/favicons/site.webmanifest" rel="manifest" />
+      <link href="public/static/favicons/favicon.ico" rel="shortcut icon" />
+      <link href="public/static/favicons/site.webmanifest" rel="manifest" />
       <link
-        href="/static/favicons/apple-touch-icon.png"
+        href="public/static/favicons/apple-touch-icon.png"
         rel="apple-touch-icon"
         sizes="180x180"
       />
       <link
-        href="/static/favicons/favicon-32x32.png"
+        href="public/static/favicons/favicon-32x32.png"
         rel="icon"
         sizes="32x32"
         type="image/png"
       />
       <link
-        href="/static/favicons/favicon-16x16.png"
+        href="public/static/favicons/favicon-16x16.png"
         rel="icon"
         sizes="16x16"
         type="image/png"

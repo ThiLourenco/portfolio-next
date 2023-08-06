12749b87bc3f53a9a95008695d2f8080a3dfe6f9
Thigo <thi.it.solutions@gmail.com>
2023-08-06 17:01:43 -0300

    chore(): change about text

--- components/sections/AboutSection.tsx
+++ components/sections/AboutSection.tsx
@@ -69,10 +69,10 @@ export const AboutSection = () => {
         <AnimationContainer customClassName="w-full flex flex-col gap-5 mb-8">
           <p className="text-base text-gray-600 dark:text-gray-400">
             Olá me chamo Thiago Lourenço, sou estudante do curso de engenharia
-            de software e desenvolvedor full-stack Javascript completamente
+            de software e desenvolvedor full-stack Javascript, sou completamente
             apaixonado por tecnologias e pela forma que podemos utiliza-lá para
             melhorar a nossa qualidade de vida. Me tornei um entusiasta com a
-            transformação da web e busco sempre a construção de solução web
+            transformação da web e busco sempre a construção de uma solução web
             rápida e eficiente, nos tempos vagos procuro sempre aprender coisas
             novas.
           </p>
